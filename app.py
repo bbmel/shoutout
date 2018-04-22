a730dc1335aa2a39e1363044551d1a7c248c21f4
Mel <mmgravier@gmail.com>
2018-04-22 13:39:54 -0400

    created the reg form

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_wtf import FlaskForm
+from wtforms import StringField, FileField, PasswordField
+from wtforms.validators import InputRequired, Length
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/mgravier/Desktop/shoutout/shoutout.db'
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'sdakfjhddsaklfjhdfjhjkcdksajfhab'
 
 
 db = SQLAlchemy(app)
@@ -20,6 +25,12 @@ class User(db.Model):
     username = db.Column(db.String(30))
     image = db.Column(db.String(100))
     password = db.Column(db.String(50))
+
+class RegisterForm(FlaskForm):
+    name = StringField('Full name', validators=[InputRequired('A full name is required.'), Length(max=100, message='Name cannot exceed 100 characters.')])
+    username = StringField('Username', validators=[InputRequired('A username is required.'), Length(max=100, message='Username cannot exceed 100 characters.')])
+    password = PasswordField('Password', validators=[InputRequired('A full name is required.')])
+    image = FileField()
 
 @app.route('/')
 def index():
